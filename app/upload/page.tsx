3696e0a69850e738826b0d4634b49450d3ce808c
majidsajadi <majidsajadi93@gmail.com>
2024-05-15 18:03:18 +0330

    feat: add upload page

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,83 +1,40 @@
 "use client";
 
-import {
-  FileUploadDropzone,
-  FileUploadFileAcceptDetails,
-  FileUploadLabel,
-  FileUploadRoot,
-  FileUploadTrigger,
-} from "@ark-ui/react";
-import { useRouter } from "next/navigation";
+import { useFormState } from "react-dom";
+import { useDropzone } from "react-dropzone";
+import { upload } from "./action";
 
 export default function Page() {
-  const router = useRouter();
+  const [state, action, pending] = useFormState(upload, { message: "" });
 
-  const handleFileAccept = async (details: FileUploadFileAcceptDetails) => {
-    try {
-      const file = details.files[0];
-      const content = await readFile(file);
+  const { getRootProps, getInputProps, fileRejections, isDragActive } =
+    useDropzone({
+      multiple: false,
+      maxFiles: 1,
+      accept: {
+        "application/json": [".json"],
+      },
+      async onDropAccepted(files) {
+        const formdata = new FormData();
+        formdata.append("manifest", files[0]);
+        await action(formdata);
+      },
+    });
 
-      const { dependencies } = JSON.parse(content);
-      if (!dependencies || !Object.keys(dependencies).length) {
-        throw new Error("No dependency found");
-      }
-
-      const dependecies = Object.entries(dependencies).map(([name, range]) => ({
-        name,
-        range,
-      }));
-
-      const query = stringifyDependencyList(dependecies);
-      const url = `/?${query}`;
-      router.push(url);
-    } catch (error) {
-      // TODO: toast
-      console.log(error);
-    }
-  };
+  const message = state.message || fileRejections[0]?.errors[0]?.message;
 
   return (
-    <FileUploadRoot
-      accept="application/json"
-      onFileAccept={handleFileAccept}
-      maxFiles={1}
-      name="manifest"
-    >
-      <FileUploadLabel>File Upload</FileUploadLabel>
-      <FileUploadDropzone>Drag your file(s) here</FileUploadDropzone>
-      <FileUploadTrigger>Choose file(s)</FileUploadTrigger>
-    </FileUploadRoot>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+
+      {pending && <div>Pending...</div>}
+
+      {message}
+    </div>
   );
-}
-
-function stringifyDependencyList(
-  dependencies: {
-    name: string;
-    range: unknown;
-  }[]
-): string {
-  const searchParams = new URLSearchParams();
-
-  dependencies.forEach(({ name, range }) => {
-    searchParams.append("dep", name + "," + range);
-  });
-
-  return searchParams.toString();
-}
-
-function readFile(file: File): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsText(file);
-
-    reader.onload = (evt) => {
-      if (evt.target?.result) {
-        resolve(evt.target.result.toString());
-      }
-    };
-
-    reader.onerror = () => {
-      reject("Error reading file");
-    };
-  });
 }

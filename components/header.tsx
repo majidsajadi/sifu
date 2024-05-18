b0131af1653f7995af6f2efc241b6b17dd94a0e1
majidsajadi <majidsajadi93@gmail.com>
2024-05-18 20:34:20 +0330

    feat: style header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Github } from "lucide-react";
 import { useTheme } from "next-themes";
 import {
   DropdownMenu,
@@ -13,6 +13,8 @@ import {
 } from "@/ui/dropdown-menu";
 import { useIsMounted } from "@/hooks/use-is-mounted";
 import { Button } from "@/ui/button";
+import styles from "./header.module.css";
+import Link from "next/link";
 
 export function Header() {
   const { theme, resolvedTheme, setTheme } = useTheme();
@@ -23,30 +25,42 @@ export function Header() {
   }
 
   return (
-    <header>
-      {/* <div>
-        The current theme is: {theme}
-        <button onClick={() => setTheme("light")}>Light Mode</button>
-        <button onClick={() => setTheme("dark")}>Dark Mode</button>
-        <button onClick={() => setTheme("system")}>System Mode</button>
-      </div> */}
-
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon">
-            {resolvedTheme === "dark" ? <Moon size={18} /> : <Sun size={18} />}
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>Theme</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
-            <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
-            <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
-            <DropdownMenuRadioItem value="system">System</DropdownMenuRadioItem>
-          </DropdownMenuRadioGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link className={styles.logo} href="/">
+          Sifu
+        </Link>
+        <Link href="/upload">Upload</Link>
+      </nav>
+      <div>
+        <Button variant="ghost" size="icon" asChild>
+          <Link href="https://github.com/majidsajadi/sifu">
+            <Github size={16} />
+          </Link>
+        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              {resolvedTheme === "dark" ? (
+                <Moon size={18} />
+              ) : (
+                <Sun size={18} />
+              )}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Theme</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+              <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="system">
+                System
+              </DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </header>
   );
 }

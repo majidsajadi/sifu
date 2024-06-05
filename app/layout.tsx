7a899dd9c676d19a47d7b92725942466dd84196f
majidsajadi <majidsajadi93@gmail.com>
2024-06-05 19:16:36 +0330

    feat: add global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,21 @@
+import NextLink from "next/link";
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
-import { Theme } from "@radix-ui/themes";
 import { Source_Code_Pro } from "next/font/google";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  IconButton,
+  Reset,
+  Text,
+  Theme,
+  Tooltip,
+  Link,
+} from "@radix-ui/themes";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { ThemeChanger } from "./theme-changer";
 
 import "./globals.css";
 
@@ -22,13 +36,50 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className={font.className}>
-      <body>
-        <ThemeProvider attribute="class">
-          <Theme accentColor="lime" grayColor="gray" radius="small">
-            {children}
-          </Theme>
-        </ThemeProvider>
-      </body>
+      <Reset>
+        <body>
+          <ThemeProvider attribute="class">
+            <Theme accentColor="lime" grayColor="gray" radius="small">
+              <Container size="4" height="100dvh">
+                <Flex height="100%" direction="column">
+                  <Box p="2" asChild>
+                    <header>
+                      <Flex align="center" justify="between" gap="2">
+                        <Heading color="lime">SIFU</Heading>
+                        <Flex align="center" gap="2">
+                          <Tooltip content="Source code">
+                            <IconButton variant="soft" color="gray" asChild>
+                              <NextLink href="https://github.com/majidsajadi/sifu">
+                                <GitHubLogoIcon />
+                              </NextLink>
+                            </IconButton>
+                          </Tooltip>
+                          <ThemeChanger />
+                        </Flex>
+                      </Flex>
+                    </header>
+                  </Box>
+                  <Box flexGrow="1" p="2" asChild>
+                    <main>{children}</main>
+                  </Box>
+                  <Box p="2" asChild>
+                    <footer>
+                      <Text color="gray" size="2">
+                        Built by{" "}
+                        <Link asChild>
+                          <NextLink href="https://github.com/majidsajadi">
+                            majidsajadi
+                          </NextLink>
+                        </Link>
+                      </Text>
+                    </footer>
+                  </Box>
+                </Flex>
+              </Container>
+            </Theme>
+          </ThemeProvider>
+        </body>
+      </Reset>
     </html>
   );
 }
---
+++ app/theme-changer.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useRef } from "react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { DropdownMenu, IconButton, Tooltip } from "@radix-ui/themes";
+
+export function ThemeChanger() {
+  const { theme, resolvedTheme, setTheme } = useTheme();
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  if (!isMounted.current) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Tooltip content="theme">
+          <IconButton variant="soft" color="gray">
+            {resolvedTheme === "dark" ? <MoonIcon /> : <SunIcon />}
+          </IconButton>
+        </Tooltip>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content size="1">
+        <DropdownMenu.RadioGroup value={theme} onValueChange={setTheme}>
+          <DropdownMenu.RadioItem value="dark">Dark</DropdownMenu.RadioItem>
+          <DropdownMenu.RadioItem value="light">Light</DropdownMenu.RadioItem>
+          <DropdownMenu.RadioItem value="system">System</DropdownMenu.RadioItem>
+        </DropdownMenu.RadioGroup>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+}

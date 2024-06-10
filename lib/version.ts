7519d8bbe30e11b1193b8dd3746dccaf75372637
majidsajadi <majidsajadi93@gmail.com>
2024-06-10 17:04:51 +0330

    refactor: remove unused data from dependency version module

--- lib/version.ts
+++ lib/version.ts
@@ -1,60 +1,19 @@
 import semver from "semver";
 import { npm } from "./npm";
 
-function getCurrentVersion(range: string): string {
-  const result = semver.minVersion(range);
-
-  if (result === null) {
-    throw new Error("Invalid range");
-  }
-
-  return result.toString();
-}
-
 function getLatestVersionStatisfies(versions: string[], range: string) {
   return semver.maxSatisfying(versions, range) ?? undefined;
 }
 
-function getGreaterVersionsSorted(versions: string[], current: string) {
-  return versions.filter((version) => semver.gt(version, current)).sort((a, b) => semver.rcompare(a, b));
-}
-
 export async function getDependencyVersion(name: string, range: string) {
-  const currentVersion = getCurrentVersion(range);
-
   const metadata = await npm.fetchDependency(name);
 
   const versions = Object.keys(metadata.versions);
   const latest = metadata["dist-tags"].latest;
   const latestSatisfies = getLatestVersionStatisfies(versions, range);
-  const greaterVersionsSorted = getGreaterVersionsSorted(versions, currentVersion);
-
-  const latestTime = metadata.time[latest];
-  const latestSatisfiesTime = latestSatisfies && metadata.time[latestSatisfies];
-
-  let availableSatisfiesCount = 0;
-  const available = greaterVersionsSorted.map((version) => {
-    const date = metadata.time[version];
-    const value = metadata.versions[version];
-
-    const satisfies = semver.satisfies(version, range);
-    if (satisfies) availableSatisfiesCount++;
-
-    return {
-      version,
-      date,
-      deprecated: !!value.deprecated,
-      satisfies,
-    };
-  });
 
   return {
     latest,
-    latestTime,
     latestSatisfies,
-    latestSatisfiesTime,
-    available,
-    availableCount: available.length,
-    availableSatisfiesCount,
   };
 }

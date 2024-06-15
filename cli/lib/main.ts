bc9e8362a5ecbe153b2dbec2b9830019b3c17713
defectivepixel <majidsajadi93@gmail.com>
2024-06-15 19:53:04 +0430

    build: setup cli project

---
+++ cli/lib/main.ts
@@ -0,0 +1,7 @@
+export type TUser = {
+  name: string;
+};
+
+export const user: TUser = {
+  name: "majid",
+};
---
+++ cli/prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
+
+export default config;
